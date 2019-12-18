50d44a007d08888313216f7f7a2b20ab72b55459
JacoDTDev <jacodtdev@gmail.com>
2019-12-18 11:26:15 +0200

    Created an mongo collection

--- server/main.js
+++ server/main.js
@@ -1,5 +1,7 @@
 import { Meteor } from 'meteor/meteor';
+import{CustomerOne} from "../imports/api/customerOne";
 
 Meteor.startup(() => {
   // code to run on server at startup
+    console.log(CustomerOne.find().fetch())
 });
