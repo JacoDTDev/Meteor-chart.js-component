c89b1f00ec4695820df7d30d107bdb1d9a059f13
JacoDTDev <jacodtdev@gmail.com>
2019-12-18 16:41:10 +0200

    Finally a f@$%

--- imports/ui/LineChart.js
+++ imports/ui/LineChart.js
@@ -1,26 +1,32 @@
 import {Meteor} from "meteor/meteor";
 import React from 'react';
 import {CustomerOne} from "../api/customerOne";
+import {Line} from 'react-chartjs-2'
 
 export default class LineChart extends React.Component{
 
     constructor(props) {
         super(props);
         this.state = {
-            data:[]
+            data:{
+                num1:[],
+                num2:[{num3:[9,8,7]},{}]
+            }
+
         };
 
     }
     componentDidMount() {
     this.tracker= Tracker.autorun(()=>{
-        const one = CustomerOne.find().fetch();
-        const three = one.map((x)=>{return x.value});
-        this.setState({data:three})
+         const one = CustomerOne.find().fetch();
+         const three = one.map((x)=>{return x.value});
+        // this.setState({data:three})
+        this.setState({data:{num2:[{num3:three}]}})
     })
     }
     render() {
 
-        const two = this.state.data;
+        const two = this.state.data.num2[0].num3;
         return(
             <div>
                 {two}
