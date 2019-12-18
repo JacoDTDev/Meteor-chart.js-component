719cfc0336c248dc75e35654a6550726be78a0a0
JacoDTDev <jacodtdev@gmail.com>
2019-12-18 12:09:10 +0200

    setting the state equal to the props passed in

--- imports/ui/LineChart.js
+++ imports/ui/LineChart.js
@@ -2,13 +2,26 @@ import {Meteor} from "meteor/meteor";
 import React from 'react';
 
 export default class LineChart extends React.Component{
+
     constructor(props) {
         super(props);
+        this.state = {
+            data:[4,5,6]
+        };
+
     }
+    componentDidMount() {
+        const one = this.props.list;
+        this.setState({data:one})
+    }
+
     render() {
+        const one = this.props.list;
+
         return(
             <div>
-                {this.props.list}
+                {one}
+                {this.state.data}
             </div>
         );
     }
